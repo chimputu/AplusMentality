dfd6f98c23ac4438bb50497d2f3bf72f2fcd7f50
chimputu <kafiswegchimputu@gmail.com>
2026-07-12 22:20:28 +0000

    still prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,21 +1,8 @@
-// lib/prisma.ts
-import { PrismaClient } from '../generated/prisma/client';
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+});
 
-// 1. Safeguard against empty strings during Next.js build optimization
-const databaseUrl = process.env.DATABASE_URL;
-const isBuilding = process.env.NEXT_PHASE === 'phase-production-build' || !databaseUrl;
-
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    // Provide a dummy valid-looking protocol fallback ONLY when building
-    accelerateUrl: isBuilding ? 'prisma://bootstrap-placeholder-string' : databaseUrl,
-    log:
-      process.env.NODE_ENV === 'development'
-        ? ['query', 'error', 'warn']
-        : ['error'],
-  });
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+export const prisma = new PrismaClient({ adapter });
